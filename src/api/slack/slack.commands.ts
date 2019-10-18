0f9fb4b1669ee22a0aea874449b3a8b0caff2f43
Gustavo <gustavolaux@live.com>
2019-10-18 01:58:16 -0300

    mongo schema refactor

--- src/api/slack/slack.commands.ts
+++ src/api/slack/slack.commands.ts
@@ -1,14 +1,42 @@
 /* eslint-disable import/no-cycle */
-// eslint-disable-next-line no-unused-vars
+/* eslint-disable no-unused-vars */
 import { BotWorker, BotkitMessage } from 'botkit';
 
+import jobs from '../../job';
 import slack from './slack.service';
-import gitlab from '../gitlab/gitlab.service';
 import factory from './slack.factory';
-import { MergeRequest } from '../mongo/mongo.service';
+import { MergeRequest, IMergeRequestModel } from '../mongo';
+import { service as gitlab, IGitlabMergeRequest } from '../gitlab';
+/* eslint-enable no-unused-vars */
+
+const saveOnMongo = async (mr: IGitlabMergeRequest, message: BotkitMessage, messageId: string) => {
+    const model = <IMergeRequestModel>{
+        'rawMergeRequest': JSON.stringify(mr),
+        'rawSlackMessage': JSON.stringify(message),
+        'url': message.text,
+        'repository': mr.repository,
+        'id': String(mr.detail.id),
+        'created': {
+            'at': new Date(mr.detail.created_at),
+            'by': mr.detail.author.username,
+        },
+        'added': {
+            'at': new Date(),
+            'by': message.user,
+        },
+        'slack': {
+            'messageId': String(messageId),
+        },
+    };
+
+    const mergeRequest = new MergeRequest(model);
+
+    return mergeRequest.save();
+};
 
 // TODO:
 // só receber de canais selecionados
+// receber mais coisas além do link (ex: #urgente)
 const handleCodeReview = async (bot: BotWorker, message: BotkitMessage) => {
     const mr = await gitlab.getMergeRequestDetail(message.text);
 
@@ -16,28 +44,16 @@ const handleCodeReview = async (bot: BotWorker, message: BotkitMessage) => {
 
     const { id } = await slack.mergeAdded(bot, message, slackMessage);
 
-    const mergeRequest = new MergeRequest({
-        'rawMergeRequest': JSON.stringify(mr),
-        'rawSlackMessage': JSON.stringify(message),
-        'created': {
-            'at': mr.detail.created_at,
-            'by': mr.detail.author.username,
-        },
-        'added': {
-            'at': new Date().toISOString(),
-            'by': message.user,
-        },
-        'slackMessageId': String(id),
-    });
+    await saveOnMongo(mr, message, id);
 
-    await mergeRequest.save();
+    if (!jobs.fetchMRUpdates.running) jobs.fetchMRUpdates.start();
 };
 
 // eslint-disable-next-line consistent-return
 const slashCommandHandler = async (bot: BotWorker, message: BotkitMessage): Promise<void> => {
-    // const start = Date.now();
-    // console.log(JSON.stringify(message));
-    // console.log(message.command);
+    const start = Date.now();
+    console.log(JSON.stringify(message));
+    console.log(message.command);
 
     switch (message.command) {
         case '/code-review':
@@ -47,7 +63,7 @@ const slashCommandHandler = async (bot: BotWorker, message: BotkitMessage): Prom
             return bot.reply(message, 'Não sei fazer isso ainda :disappointed:');
     }
 
-    // console.log(`Took ${Date.now() - start}ms`);
+    console.log(`Took ${Date.now() - start}ms`);
 };
 
 export default {
