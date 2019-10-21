61d1219332896598c47e43c1c069cedd9750d491
Gustavo <gustavolaux@live.com>
2019-10-21 18:53:09 -0300

    fix no-console rule

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import { service as slackService } from './api/slack';
+import logger from './helpers/Logger';
 
 (async () => {
     await slackService.load();
-    console.log('UP');
+    logger.info('UP');
 })();
