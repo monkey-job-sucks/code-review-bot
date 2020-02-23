e1aa342e6bdf4bbef9f9017fc1c0d05d2b82e227
Gustavo Laux <gustavolaux@MacAlugado04.local>
2020-02-22 23:42:22 -0300

    fix no-use-before-define rule

--- src/api/mongo/mongo.interfaces.ts
+++ src/api/mongo/mongo.interfaces.ts
@@ -1,6 +1,27 @@
 // eslint-disable-next-line no-unused-vars
 import { Document } from 'mongoose';
 
+interface IMergeRequestModelAnalytics {
+    upvoters: string[];
+    reviewers: string[];
+}
+
+interface IMergeRequestModelSlackChannel {
+    id: string;
+    name: string;
+}
+
+interface IMergeRequestModelSlack {
+    messageId: string;
+    reactions?: string[];
+    channel: IMergeRequestModelSlackChannel;
+}
+
+export interface IMergeRequestModelActionLog {
+    at: Date;
+    by: string;
+}
+
 export interface IMergeRequestModel extends Document {
     rawMergeRequest: string;
     rawSlackMessage: string;
@@ -20,25 +41,4 @@ export interface IMergeRequestModel extends Document {
 export interface IChannelMergeRequests {
     _id: string;
     mrs?: IMergeRequestModel[];
-}
-
-export interface IMergeRequestModelActionLog {
-    at: Date;
-    by: string;
-}
-
-interface IMergeRequestModelAnalytics {
-    upvoters: string[];
-    reviewers: string[];
-}
-
-interface IMergeRequestModelSlack {
-    messageId: string;
-    reactions?: string[];
-    channel: IMergeRequestModelSlackChannel;
-}
-
-interface IMergeRequestModelSlackChannel {
-    id: string;
-    name: string;
 }
